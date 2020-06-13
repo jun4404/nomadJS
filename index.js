770896d77de93aa77d92b885bd68d040eeb7a6e8
JUN <jun4404@gmail.com>
2020-06-14 00:57:15 +0900

    Variable

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Im Working. im JS. Im beautiful Im worth it");
+let a = 221;
+let b = a - 5;
+a = 4;
+console.log(b, a);
+// 모든 표현은 한줄에 있어야한다.
+// 1. Create
+// 2. Initialize
+// 3. Use
